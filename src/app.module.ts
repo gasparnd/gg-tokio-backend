79c859457ebcd6e60bf79ed8eb23795c8a165796
GasparND <gaspardolcemascolo@gmail.com>
2023-03-13 09:33:51 -0300

    install graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import * as Joi from 'joi';
+import config from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GeolocationModule } from './geolocation/geolocation.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { MercadopagoModule } from './mercadopago/mercadopago.module';
-import * as Joi from 'joi';
-import config from './config';
 
 @Module({
   imports: [
@@ -24,6 +28,12 @@ import config from './config';
         MERCADO_PAGO_PUBLIC_KEY: Joi.string().optional(),
         MERCADO_PAGO_ACCESS_TOKEN: Joi.string().optional(),
       }),
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+      autoSchemaFile: join(process.cwd(), './schema.gql'),
     }),
     GeolocationModule,
     DatabaseModule,
